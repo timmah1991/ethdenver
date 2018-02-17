d2a023cf91792e28a3a6599cfdc3d87f319b8f77
Thomas Kittredge <thomaskittredge@Thomass-MacBook-Pro-2.local>
2018-02-17 09:42:09 -0700

    Added framework for main control application

---
+++ codebase/maincontrol.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo starting main control script
+
+function check_blockchain {
+
+}
+
+function check_params {
+
+}
+
+function submit_bid {
+
+}
+
+function send_commands {
+
+}
+
+function reverse_commands {
+
+}
